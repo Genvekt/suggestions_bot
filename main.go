4331eede05892990dc7e69fa7f6ce91709ff1b47
Evgenia Kivotova <e.kivotova@gmail.com>
2022-06-12 18:50:05 +0300

    something

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
-import "suggestion_bot/bot"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"suggestion_bot/bot"
+)
 
 func main() {
 	SuggestionBot := bot.Bot{
 		ADMIN_CHAT_ID,
 		nil,
 	}
+	port := os.Getenv("PORT")
+	http.HandleFunc("/", handler) // each request calls handler
 
 	if err := SuggestionBot.Init(BOT_TOKEN); err != nil {
 		panic(err)
@@ -15,4 +23,9 @@ func main() {
 	if err := SuggestionBot.Run(); err != nil {
 		panic(err)
 	}
+	log.Fatal(http.ListenAndServe("localhost:"+port, nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
